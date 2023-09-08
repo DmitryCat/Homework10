5460b68cc201fe2acb1129a54c103e7048bf69aa
DimaCat <dimiiiitriy@gmail.com>
2023-09-08 18:15:10 +0300

    HM10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName;
+        fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        // Задание 2
+        System.out.println(fullName.toUpperCase());
+        // Задание 3
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
